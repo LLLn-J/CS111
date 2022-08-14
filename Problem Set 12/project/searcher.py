8a3b21b50ad607d150b0f0577c63925352d5f461
LLLn-J <90909264+LLLn-J@users.noreply.github.com>
2022-08-14 23:11:49 +0800

    2

--- Problem Set 12/project/searcher.py
+++ Problem Set 12/project/searcher.py
@@ -1,18 +1,13 @@
-#
-# searcher.py (Final project)
-#
+# Hu Xuan 8/3/2022
+# xuanh@bu.edu
+# Final project
+# searcher.py
 # classes for objects that perform state-space search on Eight Puzzles  
-#
-# name: 
-# email:
-#
-# If you worked with a partner, put his or her contact info below:
-# partner's name:
-# partner's email:
-#
+
 
 import random
 from state import *
+
 
 class Searcher:
     """ A class for objects that perform random state-space
@@ -20,8 +15,20 @@ class Searcher:
         This will also be used as a superclass of classes for
         other state-space search algorithms.
     """
-    ### Add your Searcher method definitions here. ###
 
+    ### Add your Searcher method definitions here. ###
+    def __init__(self, depth_limit):
+        """ constructs a new Searcher object by initializing some attributes
+        """
+        self.states = []
+        self.num_tested = 0
+        self.depth_limit = depth_limit
+
+    def should_add(self, state):
+        """ takes a State object called state and returns True if the called
+        Searcher should add state to its list of untested states, and False otherwise.
+        """
+        return not state.num_moves > self.depth_limit and not state.creates_cycle()
 
     def __repr__(self):
         """ returns a string representation of the Searcher object
@@ -41,10 +48,10 @@ class Searcher:
 ### Add your BFSeacher and DFSearcher class definitions below. ###
 
 
-
 def h0(state):
     """ a heuristic function that always returns 0 """
     return 0
+
 
 ### Add your other heuristic functions here. ###
 
@@ -53,8 +60,8 @@ class GreedySearcher(Searcher):
     """ A class for objects that perform an informed greedy state-space
         search on an Eight Puzzle.
     """
-    ### Add your GreedySearcher method definitions here. ###
 
+    ### Add your GreedySearcher method definitions here. ###
 
     def __repr__(self):
         """ returns a string representation of the GreedySearcher object
@@ -67,6 +74,4 @@ class GreedySearcher(Searcher):
         s += 'heuristic ' + self.heuristic.__name__
         return s
 
-
 ### Add your AStarSeacher class definition below. ###
-
